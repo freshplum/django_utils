43f89a2a547638d97860bc7fd21af37075e570f7
Sam Odio <sam@odio.com>
2011-05-15 00:45:10 -0700

    Spacing

--- misc.py
+++ misc.py
@@ -15,121 +15,120 @@ from django.conf import settings
 
 
 def custom_slugify(slug=None, name=None, length=16, num_words=2):
-	if slug and len(slug):
-		return slugify(slug).replace('-', '_')[0:length]
-	elif name and len(name):
-		return slugify(truncatewords(name, num_words)).replace('-', '_')[0:length]
-	return ''
+    if slug and len(slug):
+        return slugify(slug).replace('-', '_')[0:length]
+    elif name and len(name):
+        return slugify(truncatewords(name, num_words)).replace('-', '_')[0:length]
+    return ''
 
 
 #TODO - implement these functions to remove user_ids (and photo slugs) from the URL.
 def encrypt_id(n):
-	from Crypto.Cipher import AES
-	text = unicode(n) + '_' + settings.SECRET_SALT
-	while (len(text)%16):
-		text += '_'
-	obj = AES.new(settings.SECRET_KEY[0:32], AES.MODE_ECB)
-	return obj.encrypt(text)
+    from Crypto.Cipher import AES
+    text = unicode(n) + '_' + settings.SECRET_SALT
+    while (len(text)%16):
+        text += '_'
+    obj = AES.new(settings.SECRET_KEY[0:32], AES.MODE_ECB)
+    return obj.encrypt(text)
 
 
 #TODO - implement these functions to remove user_ids (and photo slugs) from the URL.
 def decrypt_id(ciph):
-	from Crypto.Cipher import AES
-	obj = AES.new(settings.SECRET_KEY[0:32], AES.MODE_ECB)
-	text = obj.decrypt(ciph)
-	return int(text.split('_')[0])
+    from Crypto.Cipher import AES
+    obj = AES.new(settings.SECRET_KEY[0:32], AES.MODE_ECB)
+    text = obj.decrypt(ciph)
+    return int(text.split('_')[0])
 
 
 def cache_layer(key, query, expiration=None, limit=None):
-	try:
-		query_cache = cache.get(key, None)
-	except DjangoUnicodeDecodeError:
-		query_cache = None
-		logging.basicConfig(filename='./hackerhouses.log', level=logging.DEBUG,)
-		logger = logging.getLogger('hackerhouses')
-		logger.error('Can\'t decode unicode for cache.get.  Key: ' + key + ', Query: ' + query)
-	if query_cache == None:
- 		query_cache = list(query[0:limit])
-		#Save cache for 24 hours
-		if expiration:
-			cache.set(key, query_cache, expiration)
-		else:
-			cache.set(key, query_cache)
-	return query_cache
+    try:
+        query_cache = cache.get(key, None)
+    except DjangoUnicodeDecodeError:
+        query_cache = None
+        logging.basicConfig(filename='./hackerhouses.log', level=logging.DEBUG,)
+        logger = logging.getLogger('hackerhouses')
+        logger.error('Can\'t decode unicode for cache.get.  Key: ' + key + ', Query: ' + query)
+    if query_cache == None:
+        query_cache = list(query[0:limit])
+        if expiration:
+            cache.set(key, query_cache, expiration)
+        else:
+            cache.set(key, query_cache)
+    return query_cache
 
 def do_pagination(paginate_by, GET, object_list):
-	if not 'page' in GET:
-		page = 1
-	else:
-		try:
-			page = int(GET['page'])
-		except ValueError:
-			page = 1
-	paginator = Paginator(object_list, paginate_by, orphans=3)
-	try:
-		page_obj = paginator.page(page)
-	except (EmptyPage, InvalidPage):
-		page_obj = paginator.page(paginator.num_pages)
+    if not 'page' in GET:
+        page = 1
+    else:
+        try:
+            page = int(GET['page'])
+        except ValueError:
+            page = 1
+    paginator = Paginator(object_list, paginate_by, orphans=3)
+    try:
+        page_obj = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        page_obj = paginator.page(paginator.num_pages)
 
-	if len(paginator.page_range) > 9:
-		paginator.trunc_page_range = paginator.page_range
-		first_in_set = page - 3
-		last_in_set = page + 3
-		paginator.trunc_low = False
-		paginator.trunc_high = False
-		for p in paginator.page_range:
-			if p < first_in_set:
-				paginator.trunc_low = True
-				paginator.trunc_page_range.remove(p)
-			if p > last_in_set:
-				paginator.trunc_high = True
-				paginator.trunc_page_range.remove(p)
-	return page_obj, paginator
+    if len(paginator.page_range) > 9:
+        paginator.trunc_page_range = paginator.page_range
+        first_in_set = page - 3
+        last_in_set = page + 3
+        paginator.trunc_low = False
+        paginator.trunc_high = False
+        for p in paginator.page_range:
+            if p < first_in_set:
+                paginator.trunc_low = True
+                paginator.trunc_page_range.remove(p)
+            if p > last_in_set:
+                paginator.trunc_high = True
+                paginator.trunc_page_range.remove(p)
+    return page_obj, paginator
 
 
 def print_timing(func):
-	def wrapper(*args, **kwargs):
-		t1 = time.time()
-		res = func(*args, **kwargs)
-		t2 = time.time()
-		timediff = (t2-t1)*1000.0
-		s = '%s took %0.3f ms (%s)' % (func.__name__, timediff, func.func_code if hasattr(func, 'func_code') else '')
-		if timediff > 40:
-			logging.basicConfig(filename='./hackerhouses.log', level=logging.DEBUG,)
-			logger = logging.getLogger('hackerhouses')
-			if timediff > 3000:
-			    logger.critical(s)
-			elif timediff > 750:
-			    logger.error(s)
-			elif timediff > 350:
-			    logger.warning(s)
-			elif timediff > 100:
-			    logger.info(s)
-			else:
-			    logger.debug(s)
-		return res
-	return wrapper
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        res = func(*args, **kwargs)
+        t2 = time.time()
+        timediff = (t2-t1)*1000.0
+        s = '%s took %0.3f ms (%s)' % (func.__name__, timediff, func.func_code if hasattr(func, 'func_code') else '')
+        if timediff > 40:
+            logging.basicConfig(filename='./hackerhouses.log', level=logging.DEBUG,)
+            logger = logging.getLogger('hackerhouses')
+            if timediff > 3000:
+                logger.critical(s)
+            elif timediff > 750:
+                logger.error(s)
+            elif timediff > 350:
+                logger.warning(s)
+            elif timediff > 100:
+                logger.info(s)
+            else:
+                logger.debug(s)
+        return res
+    return wrapper
 
 
 def shorten_string(s, l):
-	l = int(l)
-	if len(s) > l:
-		if l < 6:
-			s = s[:l]
-		else:
-			s = s[:(l - 3)] + '...'
-	return s
+    l = int(l)
+    if len(s) > l:
+        if l < 6:
+            s = s[:l]
+        else:
+            s = s[:(l - 3)] + '...'
+    return s
 
 
 def clean_filename(f):
-	"""
-	AWS fails on unicode image failnames.  This method removes any special characters.
-	"""
-	if f and f.name and len(f.name):
-		try: extension = f.name.rsplit('.', 1)[1]
-		except IndexError: extension = 'png'
-		f.name = custom_slugify(f.name.rsplit('.', 1)[0]) + '.' + extension
-	return f
+    """
+    AWS fails on unicode image failnames.  This method removes any special characters.
+    """
+    if f and f.name and len(f.name):
+        try: extension = f.name.rsplit('.', 1)[1]
+        except IndexError: extension = 'png'
+        f.name = custom_slugify(f.name.rsplit('.', 1)[0]) + '.' + extension
+    return f
     
 
 def get_lat_long(location):
--- tests.py
+++ tests.py
@@ -14,79 +14,79 @@ from manage_html import *
 from django.conf import settings
 
 class TestMiddleware(object):
-	"""
-	Test middleware to print out contents of GET and POST requests
-	"""
-	def __init__(self):
-		if not settings.DEBUG:
-			raise MiddlewareNotUsed
+    """
+    Test middleware to print out contents of GET and POST requests
+    """
+    def __init__(self):
+        if not settings.DEBUG:
+            raise MiddlewareNotUsed
 
-	def process_request(self, request):
-		for post_var in request.POST:
-			print post_var + ": " + request.POST[post_var]
-		return None
+    def process_request(self, request):
+        for post_var in request.POST:
+            print post_var + ": " + request.POST[post_var]
+        return None
 
-	@staticmethod
-	def show_sql(connection, show_all = False):
-		time = 0
-		count = 0
-		for q in connection.queries:
-			if show_all:
-				print q
-				print ""
-			count += 1
-			time += float(q['time'])
-		if count > 0:
-			print "Total Queries: " + str(count)
-			print "Total Time: " + str(time)
+    @staticmethod
+    def show_sql(connection, show_all = False):
+        time = 0
+        count = 0
+        for q in connection.queries:
+            if show_all:
+                print q
+                print ""
+            count += 1
+            time += float(q['time'])
+        if count > 0:
+            print "Total Queries: " + str(count)
+            print "Total Time: " + str(time)
 
-	def process_response(self, request, response):
-		TestMiddleware.show_sql(connection, show_all = False)
-		return response
+    def process_response(self, request, response):
+        TestMiddleware.show_sql(connection, show_all = False)
+        return response
 
-	def process_exception(self, request, exception):
-		import traceback
-		import sys
-		exc_info = sys.exc_info()
-		print "######################## Exception #############################"
-		print '\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))
-		print "################################################################"
-		#print repr(request)
-		#print "################################################################"
+    def process_exception(self, request, exception):
+        import traceback
+        import sys
+        exc_info = sys.exc_info()
+        print "######################## Exception #############################"
+        print '\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))
+        print "################################################################"
+        #print repr(request)
+        #print "################################################################"
 
 
 ###################
 # utils.py tests #
 ##################
 class UtilsTestCase(TestCase):
-	def setUp(self):
-		self.client = Client()
-		self.login = "test_login"
-		self.password = 'test_pass1234'
+    def setUp(self):
+        self.client = Client()
+        self.login = "test_login"
+        self.password = 'test_pass1234'
 
-		self.text = '<a href="http://test.com">test.com</a><div><strong>this <i>is</i> a div</strong> http://google.com: http://msn.com, http://yahoo.com http://example.com \'http://tehcrowd.com\'</div> @omarish @tehcrowd_test'
-		self.stripped_text = '<a href="http://test.com">test.com</a><strong>this <i>is</i> a div</strong> http://google.com: http://msn.com, http://yahoo.com http://example.com \'http://tehcrowd.com\' @omarish @tehcrowd_test'
-		self.convert_links_text = '<a href="http://test.com">test.com</a><div><strong>this <i>is</i> a div</strong> <a href="http://google.com">http://google.com</a>: <a href="http://msn.com">http://msn.com</a>, <a href="http://yahoo.com">http://yahoo.com</a> <a href="http://example.com">http://example.com</a> \'http://tehcrowd.com\'</div> <a href="http://twitter.com/omarish/">@omarish</a> <a href="http://twitter.com/tehcrowd_test/">@tehcrowd_test</a>'
+        self.text = '<a href="http://test.com">test.com</a><div><strong>this <i>is</i> a div</strong> http://google.com: http://msn.com, http://yahoo.com http://example.com \'http://tehcrowd.com\'</div> @omarish @tehcrowd_test'
+        self.stripped_text = '<a href="http://test.com">test.com</a><strong>this <i>is</i> a div</strong> http://google.com: http://msn.com, http://yahoo.com http://example.com \'http://tehcrowd.com\' @omarish @tehcrowd_test'
+        self.convert_links_text = '<a href="http://test.com">test.com</a><div><strong>this <i>is</i> a div</strong> <a href="http://google.com">http://google.com</a>: <a href="http://msn.com">http://msn.com</a>, <a href="http://yahoo.com">http://yahoo.com</a> <a href="http://example.com">http://example.com</a> \'http://tehcrowd.com\'</div> <a href="http://twitter.com/omarish/">@omarish</a> <a href="http://twitter.com/tehcrowd_test/">@tehcrowd_test</a>'
 
-	def testShortenStrong(self):
-		print '\nutils > misc.py > test shorten_string'
-		n = 1
-		chars = string.letters + string.digits + '!@#$%^&*() _-"][{.`~}'
-		while n < 33:
-			s = ''
-			for i in range(32):
-				s = s + choice(chars)
-			self.assertEqual(len(shorten_string(s, n)), n)
-			n += 1
+    def testShortenStrong(self):
+        print '\nutils > misc.py > test shorten_string'
+        n = 1
+        chars = string.letters + string.digits + '!@#$%^&*() _-"][{.`~}'
+        while n < 33:
+            s = ''
+            for i in range(32):
+                s = s + choice(chars)
+            self.assertEqual(len(shorten_string(s, n)), n)
+            n += 1
 
-	def testConvertLinks(self):
-		print '\nutils > manage_html.py > test convert_links()'
-		self.assertEqual(convert_links(self.text), self.convert_links_text)
+    def testConvertLinks(self):
+        print '\nutils > manage_html.py > test convert_links()'
+        self.assertEqual(convert_links(self.text), self.convert_links_text)
 
-	def testStripHtml(self):
-		print '\nutils > manage_html.py > test strip()'
-		self.assertEqual(strip(self.text), self.stripped_text)
+    def testStripHtml(self):
+        print '\nutils > manage_html.py > test strip()'
+        self.assertEqual(strip(self.text), self.stripped_text)
 
-	def testSerializeObject(self):
-		print '\nnutils > misc.py >  test serialize_object'
-		#Haven't yet bothered to teset this...
+    def testSerializeObject(self):
+        print '\nnutils > misc.py >  test serialize_object'
+        #Haven't yet bothered to teset this...
