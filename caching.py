d7c6e4dc4c85e2cbc96a1a61b8671b689949209e
Sam Odio <sam@odio.com>
2011-06-23 21:16:27 -0700

    Simple per-object cacching

---
+++ caching.py
@@ -0,0 +1,43 @@
+from django.core.cache import cache
+from django.db.models.signals import post_save, pre_delete
+from django.db.models.query import QuerySet
+from django.db import models
+
+#Simple GET caching.  Cribbed from http://www.eflorenzano.com/blog/post/drop-dead-simple-django-caching/
+
+def key_from_instance(instance):
+    opts = instance._meta
+    return '%s.%s:%s' % (opts.app_label, opts.module_name, instance.pk)
+
+def post_save_cache(sender, instance, **kwargs):
+    cache.set(key_from_instance(instance), instance)
+post_save.connect(post_save_cache)
+
+def pre_delete_uncache(sender, instance, **kwargs):
+    cache.delete(key_from_instance(instance))
+pre_delete.connect(pre_delete_uncache)
+
+class SimpleCacheQuerySet(QuerySet):
+    def filter(self, *args, **kwargs):
+        pk = None
+        for val in ('pk', 'pk__exact', 'id', 'id__exact'):
+            if val in kwargs:
+                pk = kwargs[val]
+                break
+        if pk is not None:
+            opts = self.model._meta
+            key = '%s.%s:%s' % (opts.app_label, opts.module_name, pk)
+            obj = cache.get(key)
+            if obj == None:
+                res = super(SimpleCacheQuerySet, self).filter(*args, **kwargs)
+                cache.set(key, res)
+                print "set cache %s / %s" % (key, res)
+                return res
+            else:
+                print "got cache %s / %s" % (key, obj)
+                self._result_cache = [obj]
+        return super(SimpleCacheQuerySet, self).filter(*args, **kwargs)
+
+class SimpleCacheManager(models.Manager):
+    def get_query_set(self):
+        return SimpleCacheQuerySet(self.model)
