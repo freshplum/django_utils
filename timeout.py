560647576155dc64c44f1c2d85c205f88904a9de
Sam Odio <sam@odio.com>
2011-07-26 01:00:08 -0700

    Timeout function

---
+++ timeout.py
@@ -0,0 +1,25 @@
+import signal
+from misc import CustomError
+
+def handler(signum, frame):
+    raise CustomError("Timeout")
+
+signal.signal(signal.SIGALRM, handler)
+
+def run_func(timeout, func, *args, **kwargs):
+    """
+    This will run the function passed.
+    And will raise an exception if the
+    the function takes more than timeout
+    (specified in miliseconds).
+
+    """
+
+    signal.setitimer(signal.ITIMER_REAL, float(timeout)/1000.0)
+    result = func(*args, **kwargs)
+    signal.setitimer(signal.ITIMER_REAL, 0)
+    return result
+
+def infinite_loop():
+    while True:
+        print 'test'
