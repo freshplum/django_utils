7bf31f977e9c351b1ed6e41d6e23cc0bf73c9a6d
Sam Odio <sam@odio.com>
2011-07-26 23:51:34 -0700

    don't import settings from project

--- render.py
+++ render.py
@@ -12,22 +12,22 @@ from django.contrib.auth import logout
 from django.template import Template
 
 #From Project:
-from settings import LOGIN_URL, LOGOUT_URL
+from django.conf import settings
 
 #allways use this when rendering templates (it executes required functions) before rendering the template
 #TODO - include these in generic views...
 def template(template_name, locals_dict=None, status=200):
     no_redirects = (
-        LOGIN_URL,
-        LOGOUT_URL,
+        settings.LOGIN_URL,
+        settings.LOGOUT_URL,
     )
-    
+
     #these functions should be executed before every page render:
     if 'next' in locals_dict['request'].GET:
         locals_dict['request'].session['prev_page'] = locals_dict['request'].GET['next']
     elif not 'forget_path' in locals_dict:
         locals_dict['request'].session['prev_page'] = locals_dict['request'].path
-    
+
     if 'prev_page' in locals_dict['request'].session and (locals_dict['request'].session['prev_page'] in no_redirects or '/api/' in locals_dict['request'].session['prev_page']):
         locals_dict['request'].session['prev_page'] = '/'
 
@@ -35,7 +35,7 @@ def template(template_name, locals_dict=None, status=200):
     if locals_dict['request'].user.is_authenticated():
         locals_dict['user'] = locals_dict['request'].user
     return render_wrapper(template_name, locals_dict, status=status)
-    
+
 def previous(request):
     redirect = ('/' + request.session['prev_page'].lstrip('/') if 'prev_page' in request.session else urlresolvers.reverse('index'))
     return HttpResponseRedirect(redirect)
