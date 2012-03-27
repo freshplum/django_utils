fb6e69b58e4407b94d3ac0cf73d4d5b59e2152fa
Nick Alexander <nick2k5@gmail.com>
2012-03-27 06:41:37 -0700

    add sql resource

---
+++ sql.py
@@ -0,0 +1,15 @@
+from django.db import connections, transaction, backend
+
+def select_sql(sql, db='tron'):
+    cursor = connections[db].cursor()
+    cursor.execute(sql)
+    transaction.commit_unless_managed(using=db)
+    return dictfetchall(cursor)
+
+def dictfetchall(cursor):
+    "Returns all rows from a cursor as a dict"
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
